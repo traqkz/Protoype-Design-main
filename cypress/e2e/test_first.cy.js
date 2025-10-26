32a2ca8f29201b4e0a293d8e241b9f993cb8e755
jonah <jonah@jonah.com>
2025-10-25 22:13:21 -0400

    Remove unnecessary control icon clicks from Cypress test for improved test efficiency.

--- cypress/e2e/test_first.cy.js
+++ cypress/e2e/test_first.cy.js
@@ -2,9 +2,5 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('/')
     cy.get('button.play-button').click();
-    cy.get('li:nth-child(1) span.control-icon').click();
-    cy.get('li:nth-child(2) span.control-icon').click();
-    cy.get('li:nth-child(3) span.control-icon').click();
-    cy.get('li:nth-child(4) span.control-icon').click();
   })
 })
